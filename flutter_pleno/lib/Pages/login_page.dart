8f42b010957a620f78dd9cb55bd7efd146c5f224
Vinícius Souza <dev.souzavinicius@gamil.com>
2024-02-28 16:22:28 -0300

    styles: estilos tela de login finalizado

--- flutter_pleno/lib/Pages/login_page.dart
+++ flutter_pleno/lib/Pages/login_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key); // Correção do construtor
+
+  void test() {
+    print('test');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
-      body:const Center(
-        child: Text(
-          'Login page Works',
-          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Login page Works',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: test,
+              child: Text('Logar com Google'),
+            )
+          ],
         ),
       ),
     );
